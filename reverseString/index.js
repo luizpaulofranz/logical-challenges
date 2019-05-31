d4a6ba57f312d5ffd7b2fee81063c6da66f3d3cf
Luiz Paulo Franz <luizpaulofranz@gmail.com>
2019-05-31 07:56:28 -0300

    Adds more algorithms on reverse string problem.

--- reverseString/index.js
+++ reverseString/index.js
@@ -10,9 +10,48 @@ function reverseString1(text) {
 }
 
 // using chain of built-in methods and ES6 spread operator
-function reverseString(text) {
+function reverseString2(text) {
     return [...text].reverse().join("");
 }
 
+// using commom for loop
+function reverseString3(text) {
+    let ret = "";
+    for(let i = text.length -1; i >= 0; i--) {
+        ret += text[i];
+    }
+    return ret;
+}
+
+// for ... of aproach
+function reverseString4(text) {
+    let ret = '';
+
+    //text.split('').forEach(element => {
+    //    ret = element + ret
+    //});
+
+    // looks like foreach
+    for (myChar of text) {
+        ret = myChar + ret;
+    }
+
+    return ret;
+}
+
+// using recursion
+function reverseString5(text) {
+    if (text === "") {
+        return ""
+    } else {
+        // substr params: start index and size. It can receive negative params to get chars from behind
+        return reverseString(text.substr(1)) + text[0]
+    }
+}
+
+// using JS ES6 reducer
+function reverseString(text) {
+    return text.split("").reduce((acc, char) => char + acc, '')
+}
 
 module.exports = reverseString
